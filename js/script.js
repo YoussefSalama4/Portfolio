2363b2f37b7ae547e102748dd670b04f33aea1d6
YoussefSalama4 <ahmdsalama4@gmail.com>
2024-09-09 20:25:20 +0300

    update portfolio

--- js/script.js
+++ js/script.js
@@ -43,14 +43,16 @@ ScrollReveal({
   duration: 2000,
   delay: 200,
 });
-ScrollReveal().reveal(".home-content, .heading", { origin: "top" });
+ScrollReveal().reveal(".home-content, .heading", {
+  origin: "top",
+});
 ScrollReveal().reveal(
-  ".home-img, .services-container, .portfolio-box, .contact form",
+  ".home-img, .services-container, .portfolio-box, .contact form ",
   {
     origin: "bottom",
   }
 );
-ScrollReveal().reveal(".home-content h1, .about-img", {
+ScrollReveal().reveal(".home-content h1, .about-img, .portfolio-header", {
   origin: "left",
 });
 ScrollReveal().reveal(".home-content p, .about-content", { origin: "right" });
